f38271291089964916797e849cbb2c3a0bf934c2
zacfrappier <zacfrappier@gmail.com>
2025-11-21 00:56:04 -0800

    added matricies and arrarys

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -8,7 +8,7 @@ Lab 3
 #include <limits.h>
 
 // Declare dynamic arrays/vectors and global variables
-int placeholder = INT_MIN;   //just used for things i need to change later
+int placeholder = INT_MIN;   //just used for things i GraphNeed to change later
 #define MAX_VERTICES 5 // 5x5 matrix
 
 //make graph
@@ -32,12 +32,45 @@ resources 2 x r matrix OR vector
 
 available 2 x r matrix OR vector
 
-allocated p x r matrix 
+GraphAllocated p x r matrix 
 
-need p x r matrix
+GraphNeed p x r matrix
 
 */
+int *resources;
+int *available;
+int size = 5;
 
+resources =(int *)malloc(size * sizeof(int));
+resources =(MAX_VERTICES *)malloc(MAX_VERTICES * sizeof(MAX_VERTICES));
+
+typedef struct GraphAllocated{
+	int n;
+	int adj_matrix[MAX_VERTICES][MAX_VERTICES];
+}GraphAllocated;
+
+void initialize_graph(GraphAllocated* g, int vertices){
+    g->n = vertices;
+    for (int i = 0; i < g->n; i++){
+        for(int j = 0; j < g->n; j++){
+            g->adj_matrix[i][j] = 0; //no edges
+        }
+    }
+}
+
+typedef struct GraphNeed{
+	int n;
+	int adj_matrix[MAX_VERTICES][MAX_VERTICES];
+}GraphNeed;
+
+void initialize_graph(GraphNeed* g, int vertices){
+    g->n = vertices;
+    for (int i = 0; i < g->n; i++){
+        for(int j = 0; j < g->n; j++){
+            g->adj_matrix[i][j] = 0; //no edges
+        }
+    }
+}
 
 
 //*********************************************************
@@ -73,9 +106,9 @@ void claim_graph() {
 	// for each resource, prompt for number of units, set resource and available vectors indices
 	// prompt for number of processes
 	// allocate memory for arrays
-	// for each process, for each resource, prompt for maximum number of units requested by process, update maxclaim and need arrays  
-	// for each process, for each resource, prompt for number of resource units allocated to process 
-	// print resource vector, available vector, maxclaim array, allocated array, need array 
+	// for each process, for each resource, prompt for maximum number of units requested by process, update maxclaim and GraphNeed arrays  
+	// for each process, for each resource, prompt for number of resource units GraphAllocated to process 
+	// print resource vector, available vector, maxclaim array, GraphAllocated array, GraphNeed array 
 	return;
 }
 
@@ -84,11 +117,11 @@ void claim_graph() {
 void request_resources() {
 	// declare local variables
 	// prompt for process, resource, and number of units requested
-	// if enough units available and request is less than need
+	// if enough units available and request is less than GraphNeed
 		// reduce number of available units
-		// increase number of allocated units
-		// reduce number of needed units
-		// print updated available, allocated, and need vectors/matrices
+		// increase number of GraphAllocated units
+		// reduce number of GraphNeeded units
+		// print updated available, GraphAllocated, and GraphNeed vectors/matrices
 	// else
 		//print message that request was denied
 	return;
@@ -99,11 +132,11 @@ void request_resources() {
 void release_resources() {
 	// declare local variables
 	// prompt for process, resource, and number of units requested
-	// if enough units allocated
+	// if enough units GraphAllocated
 		// increase number of available units
-		// reduce number of allocated units
-		// increase number of needed units
-		// print updated available, allocated, and need vectors/matrices
+		// reduce number of GraphAllocated units
+		// increase number of GraphNeeded units
+		// print updated available, GraphAllocated, and GraphNeed vectors/matrices
 	// else
 		//print message that release cannot be performed
 	return;
@@ -117,14 +150,14 @@ void safe_check() {
 	// while not all processes are processed
 		// for each process  
 			// if process has not been processed yet 	 
-				// print message comparing need vector with available vector
+				// print message comparing GraphNeed vector with available vector
 				// for each resource 
-					// check for safe processing by comparing process' need vector to available vector 
+					// check for safe processing by comparing process' GraphNeed vector to available vector 
 	      			// if each resource is available 
 					// print message that process can be processed
 					// update number of available units of resource 
 					// for each resource 
-						//free all resources allocated to process 
+						//free all resources GraphAllocated to process 
 						// increment number of sequenced processes 
 				// else print message that process cannot be processed
 	//if (no process was processed in the final round of the for-loop)
@@ -143,6 +176,9 @@ void exit_free_mem() {
 
 //*************************************************************
 int main() {
+	
+
+
     printf("This is Lab 3, showing hdeadlock avoidance through bankers algorithm\n");
 
 	// declare local vars 
@@ -150,7 +186,7 @@ int main() {
 	// while user has not chosen to quit 
     do{
         printf("--- Menu --- \n");
-        printf("1) Enter Claim graph (need) \n"); 
+        printf("1) Enter Claim graph (GraphNeed) \n"); 
         printf("2) Request resource (request)\n"); 
         printf("3) Release resource (allocation)\n"); 
         printf("4) Determine safe sequence\n"); 
@@ -159,7 +195,7 @@ int main() {
 
         switch(choice){
             case 1:
-                printf("you've selected to enter claim graph of resources needed\n");
+                printf("you've selected to enter claim graph of resources GraphNeeded\n");
                 claim_graph();
                 break;
             case 2:
@@ -167,15 +203,15 @@ int main() {
                 request_resources();
                 break;
             case 3:
-                printf("you've selected to enter claim graph of resources needed\n");
+                printf("you've selected to enter claim graph of resources GraphNeeded\n");
                 release_resources();
                 break;
             case 4:
-                printf("you've selected to enter claim graph of resources needed\n");
+                printf("you've selected to enter claim graph of resources GraphNeeded\n");
                 safe_check();
                 break;
             case 5:
-                printf("you've selected to enter claim graph of resources needed\n");
+                printf("you've selected to enter claim graph of resources GraphNeeded\n");
                 exit_free_mem();
                 break;
             default:
